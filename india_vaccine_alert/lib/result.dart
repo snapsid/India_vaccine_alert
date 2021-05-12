f44b091a53dbe634eb0e9529e77c320fdf64eb44
snapsid <siddharthchopra555@gmail.com>
2021-05-12 11:07:24 +0530

    list view added

--- india_vaccine_alert/lib/result.dart
+++ india_vaccine_alert/lib/result.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,7 +10,7 @@ class MyResult extends StatefulWidget {
 }
 
 class _MyResultState extends State<MyResult> {
-  var pincode = "457001";
+  var pincode = "452001";
   var date = "12-05-21";
   getVac(pin, date) async {
     var url = Uri.parse(
@@ -36,6 +37,10 @@ class _MyResultState extends State<MyResult> {
     var sessionCount = session.length;
     print(sessionCount);
 
+    setState(() {
+      listCount = sessionCount;
+    });
+
     if (sessionCount > 0) {
       var centerId = session[0]['center_id'];
       var nameCenter = session[0]['name'];
@@ -47,6 +52,9 @@ class _MyResultState extends State<MyResult> {
       var availableCapacity = session[0]['available_capacity'];
       var vaccineType = session[0]['vaccine'];
 
+      setState(() {
+        vacType = vaccineType;
+      });
       print(minAge);
       print(vaccineType);
     }
@@ -57,6 +65,83 @@ class _MyResultState extends State<MyResult> {
     // TODO: implement initState
     super.initState();
   }
+
+  myListView(vacType) {
+    return Card(
+      elevation: 5,
+      child: Container(
+          margin: EdgeInsets.only(left: 30, top: 20),
+          height: 150,
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    '$vacType',
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 21,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    'Name:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    'Address:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    'City:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    'State:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+              Container(
+                  margin: EdgeInsets.only(bottom: 10, right: 20),
+                  alignment: Alignment.bottomRight,
+                  child: Text(
+                    'Min Age: ',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                  )),
+              Container(
+                  margin: EdgeInsets.only(bottom: 10, right: 20),
+                  alignment: Alignment.centerRight,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Available',
+                        style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800),
+                      ),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        '5',
+                        style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  )),
+            ],
+          )),
+    );
+  }
+
+  var listCount = 0;
+  var vacType = "";
 
   @override
   Widget build(BuildContext context) {
@@ -71,6 +156,18 @@ class _MyResultState extends State<MyResult> {
               })
         ],
         backgroundColor: Colors.teal,
+      ),
+      body: Container(
+        child: ListView.builder(
+          shrinkWrap: true,
+          // controller: scrollcontorller,
+
+          padding: EdgeInsets.only(bottom: 20),
+          itemCount: listCount,
+          itemBuilder: (BuildContext context, int index) {
+            return myListView(vacType);
+          },
+        ),
       ),
     );
   }
