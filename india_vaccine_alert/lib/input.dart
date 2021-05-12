6a43f602e5283dd6c56051a709cf41372f00655a
snapsid <siddharthchopra555@gmail.com>
2021-05-12 10:10:55 +0530

    input ui done

--- india_vaccine_alert/lib/input.dart
+++ india_vaccine_alert/lib/input.dart
@@ -10,7 +10,47 @@ class _MyInputState extends State<MyInput> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.teal,
         title: Text('Enter details'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              child: TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {},
+                decoration: InputDecoration(
+                    filled: true,
+                    hintText: "Enter pincode",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Material(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.teal,
+              elevation: 5,
+              child: MaterialButton(
+                child: Text(
+                  'Search',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+                minWidth: 130,
+                height: 40,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- india_vaccine_alert/lib/main.dart
+++ india_vaccine_alert/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:india_vaccine_alert/input.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: 'input',
     routes: {
       'input': (context) => MyInput(),
